421229f9418f0c6c0d32e2e20a618f455aa394db
Gaddam Punnam Chandra <gaddampunnamchandra@gmail.com>
2026-08-14 10:47:32 +0530

    added randomUser button functionality

--- frontend/index.js
+++ frontend/index.js
@@ -24,3 +24,26 @@ function toggleUser(){
     userGender.innerHTML=users[curId].gender;
     userImage.src=users[curId].image;
 }
+
+function randomUser(){
+    fetch("https://randomuser.me/Api")
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            var userName=document.getElementById("user-name");
+            var userGender=document.getElementById("user-gender");
+            var userImage=document.getElementById("user-image");
+
+            var newuserName=data.results[0].name.first + " "+data.results[0].name.last;
+            var newuserGender=data.results[0].gender;
+            var newuserImage=data.results[0].picture.large;
+
+            userName.innerHTML=newuserName;
+            userGender.innerHTML=newuserGender;
+            userImage.src=newuserImage;
+        })
+        .catch(function(err){
+            console.log("error occured:"+ err);
+        })
+}
